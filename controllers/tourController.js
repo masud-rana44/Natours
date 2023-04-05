ecaac8e1a751bd1ece1619869191c7ddd94c4716
masud-rana44 <mdmasudrana4488@gmail.com>
2023-04-06 01:31:49 +0600

    Aggragation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,78 +1,23 @@
 const Tour = require('../models/tourModels');
+const APIFeatures = require('../utils/apiFeatures');
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
-// );
-
-// exports.checkId = (req, res, next, val) => {
-//   const id = val * 1;
-//   const tour = tours.find((el) => el.id === id);
-
-//   if (!tour) {
-//     return res.status(404).send({
-//       staus: 'fail',
-//       message: 'Invalid ID',
-//     });
-//   }
-
-//   next();
-// };
-
-// exports.checkBody = (req, res, next) => {
-//   if (!req.body.name || !req.body.price) {
-//     return res.status(400).json({
-//       status: 'fail',
-//       message: 'Missing name or price',
-//     });
-//   }
-
-//   next();
-// };
+exports.topToursAlieas = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  // req.query.fields = 'name,price,difficulty,ratingsAverage,summary';
+  next();
+};
 
 exports.getTours = async (req, res) => {
   try {
-    console.log(req.query);
-
-    // BUILD QUERY
-    //  1A) Filtering
-    const queryObj = { ...req.query };
-    const excludeFilds = ['sort', 'limit', 'page', 'filds'];
-    excludeFilds.forEach((el) => delete queryObj[el]);
-
-    // 1B) Advance filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    if (req.query.sort) {
-      // sort('price ratingsAverage')
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3) Field limiting
-    if (req.query.fields) {
-      // select('name duration difficulty')
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-    // const query = Tour.find()
-    //   .where('difficulty')
-    //   .equals('easy')
-    //   .where('duration')
-    //   .equals(5);
-
-    // Tour.find({ difficulty: 'easy', duration: { $gte: 5 }})
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
@@ -158,6 +103,65 @@ exports.deleteTour = async (req, res) => {
     res.status(204).json({
       status: 'deleted',
       message: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([{}]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
     });
   } catch (err) {
     res.status(404).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,6 +5,9 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  topToursAlieas,
+  getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const router = express.Router();
@@ -12,6 +15,9 @@ const router = express.Router();
 // Perameter middleware
 // router.param('id', checkId);
 
+router.route('/top-5-cheap').get(topToursAlieas, getTours);
+router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/').get(getTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
