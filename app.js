e94543ffd844dbda3b85cf41f6f7aa34bb8e5cd6
masud-rana44 <mdmasudrana4488@gmail.com>
2023-04-07 22:46:11 +0600

    global error handler

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -14,11 +16,6 @@ app.use(express.static(`${__dirname}/public`));
 
 // custom
 app.use((req, res, next) => {
-  console.log('Hello form the middleware😍');
-  next();
-});
-
-app.use((req, res, next) => {
   req.createdAt = new Date().toISOString();
   next();
 });
@@ -26,5 +23,20 @@ app.use((req, res, next) => {
 // ROUTES (Mounting)
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -12,7 +12,7 @@ class APIFeatures {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    this.query.find(JSON.parse(queryStr));
+    this.query = this.query.find(JSON.parse(queryStr));
 
     return this;
   }
@@ -20,9 +20,9 @@ class APIFeatures {
   sort() {
     if (this.queryString.sort) {
       const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query.sort(sortBy);
+      this.query = this.query.sort(sortBy);
     } else {
-      this.query.sort('id');
+      this.query = this.query.sort('id');
     }
 
     return this;
@@ -31,9 +31,9 @@ class APIFeatures {
   limitFields() {
     if (this.queryString.fields) {
       const fields = this.queryString.fields.split(',').join(' ');
-      this.query.select(fields);
+      this.query = this.query.select(fields);
     } else {
-      this.query.select('-__v');
+      this.query = this.query.select('-__v');
     }
 
     return this;
@@ -44,7 +44,7 @@ class APIFeatures {
     const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
 
-    this.query.skip(skip).limit(limit);
+    this.query = this.query.skip(skip).limit(limit);
 
     return this;
   }
